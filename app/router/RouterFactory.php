f80c53a65b9a319e0e01a41aa4353f11ca807251
Filip Procházka <filip@prochazka.su>
2014-04-21 23:09:53 +0200

    Router redirects to https

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -14,12 +14,29 @@ class RouterFactory
 {
 
 	/**
+	 * @var bool
+	 */
+	private $productionMode;
+
+
+
+	public function __construct($productionMode)
+	{
+		$this->productionMode = (bool) $productionMode;
+	}
+
+
+
+	/**
 	 * @return \Nette\Application\IRouter
 	 */
 	public function createRouter()
 	{
 		$router = new RouteList();
-		$router[] = new Route('<presenter>/<action>', 'Categories:default');
+		$flags = $this->productionMode ? Route::SECURED : 0;
+
+		$router[] = new Route('<presenter>/<action>', 'Categories:default', $flags);
+
 		return $router;
 	}
 
