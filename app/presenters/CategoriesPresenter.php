bc99967a4bf9df32ba6b71274888c7ff577c34a7
Filip Procházka <filip@prochazka.su>
2014-04-23 17:03:36 +0200

    Fixed last post at Categories

--- app/presenters/CategoriesPresenter.php
+++ app/presenters/CategoriesPresenter.php
@@ -26,31 +26,47 @@ class CategoriesPresenter extends BasePresenter
 		$totalCountQb = $questions->createQueryBuilder('tp')
 			->select('COUNT(tp.id)')
 			->andWhere('tp.spam = FALSE AND tp.deleted = FALSE')
-			->innerJoin('tp.category', 'tpc')
-			->andWhere('tpc.id = c2.id');
+			->innerJoin('tp.category', 'tpc')->andWhere('tpc.id = c2.id');
 
 		$solvedCountQb = $questions->createQueryBuilder('sp')
 			->select('COUNT(sp.id)')
 			->andWhere('sp.spam = FALSE AND sp.deleted = FALSE')
 			->innerJoin('sp.solution', 'sps') // has solution
-			->innerJoin('sp.category', 'spc')
-			->andWhere('spc.id = c2.id');
+			->innerJoin('sp.category', 'spc')->andWhere('spc.id = c2.id');
 
 		$categories = $this->em->getDao(Category::class);
-		$qb = $categories->createQueryBuilder('c1')
+		$categoriesQb = $categories->createQueryBuilder('c1')
 			->where('c1.parent IS NULL')
 			->leftJoin('c1.children', 'c2')->addSelect('c2')
-			->addSelect('(' . $totalCountQb->getDQL() . ') as posts_count')
-			->addSelect('(' . $solvedCountQb->getDQL() . ') as solved_count')
-			->leftJoin(Question::class, 'l', Join::WITH, 'c2 = l.category AND l.spam = FALSE AND l.deleted = FALSE')
-				->addSelect('l.id as l_id, l.createdAt as l_createdAt, l.title as l_title')
-			->leftJoin('l.author', 'li')
-			->leftJoin('li.user', 'lu')->addSelect('lu.name as l_author_name')
+			->addSelect("($totalCountQb) as posts_count")
+			->addSelect("($solvedCountQb) as solved_count")
 			->orderBy('c1.position', 'ASC')->addOrderBy('c2.position', 'ASC')
 			->groupBy('c1.id, c2.id');
 
-		$result = array_map('Nette\ArrayHash::from', $qb->getQuery()->getScalarResult());
+		$result = array_map('Nette\ArrayHash::from', $categoriesQb->getQuery()->getScalarResult());
 		$this->template->categories = Nette\Utils\Arrays::associate($result, 'c1_id|c2_id->');
+
+		$db = $this->em->getConnection();
+		$lastPostIds = $db->executeQuery("
+			SELECT c.id, (
+				SELECT p.id FROM forum_posts p
+				INNER JOIN forum_category pc ON p.category_id = pc.id
+				WHERE p.spam = 0 AND p.deleted = 0 AND p.type IN ('question') AND pc.id = c.id
+				ORDER BY p.created_at DESC LIMIT 1
+			) as last_post_id FROM forum_category c WHERE c.id IN (?)
+		", [array_map(function ($row) { return $row->c2_id; }, $result)], [$db::PARAM_INT_ARRAY]);
+		$lastPostIds = array_filter(Nette\Utils\Arrays::associate($lastPostIds->fetchAll(), 'id=last_post_id'));
+
+		$lastQuestionQb = $questions->createQueryBuilder('lq')
+			->select('lq.id as l_id, lq.createdAt as l_createdAt, lq.title as l_title')
+			->andWhere('lq.spam = FALSE AND lq.deleted = FALSE')
+			->innerJoin('lq.category', 'lqc')->addSelect('lqc.id as c_id')
+			->leftJoin('lq.author', 'li')
+			->leftJoin('li.user', 'lu')->addSelect('lu.name as l_author_name')
+			->andWhere('lq.id IN (:ids)')->setParameter('ids', $lastPostIds);
+
+		$lastQuestions = array_map('Nette\ArrayHash::from', $lastQuestionQb->getQuery()->getArrayResult());
+		$this->template->lastQuestions = Nette\Utils\Arrays::associate($lastQuestions, 'c_id->');
 	}
 
 }
