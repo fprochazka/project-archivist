8ae96bbbc76f80095c150fc4c2be79390cc266b3
Filip Procházka <filip@prochazka.su>
2014-04-28 20:21:34 +0200

    Implemented gravatars [Closes #19]

--- app/ForumModule/presenters/BasePresenter.php
+++ app/ForumModule/presenters/BasePresenter.php
@@ -10,6 +10,7 @@
 
 namespace Archivist\ForumModule;
 
+use IPub;
 use Kdyby;
 use Nette;
 
@@ -20,5 +21,25 @@ use Nette;
  */
 abstract class BasePresenter extends \Archivist\BasePresenter
 {
+
+	use IPub\Gravatar\TGravatar;
+
+
+
+	protected function createTemplate($class = NULL)
+	{
+		/** @var Nette\Bridges\ApplicationLatte\Template|\stdClass $template */
+		$template = parent::createTemplate($class);
+
+		// Add gravatar to template
+		$template->_gravatar = $this->gravatar;
+
+		// Register template helpers
+		$template->addFilter('gravatar', function ($email, $size = NULL) {
+			return $this->gravatar->buildUrl($email, $size);
+		});
+
+		return $template;
+	}
 
 }
