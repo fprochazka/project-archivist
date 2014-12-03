fd15fee871c9e702199d4a615685f7e1d04405c3
Filip Procházka <filip@prochazka.su>
2014-12-03 01:30:27 +0100

    refactored app/bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,20 +2,22 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$configurator = new Nette\Configurator;
+return call_user_func(function () {
+	$configurator = new Nette\Configurator;
 
-//$configurator->setDebugMode(TRUE);  // debug mode MUST NOT be enabled on production server
-$configurator->enableDebugger(__DIR__ . '/../log');
-$configurator->setTempDirectory(__DIR__ . '/../temp');
+	//$configurator->setDebugMode(TRUE);  // debug mode MUST NOT be enabled on production server
+	$configurator->enableDebugger(__DIR__ . '/../log');
+	$configurator->setTempDirectory(__DIR__ . '/../temp');
 
-if ($configurator->isDebugMode()) {
-	$configurator->createRobotLoader()
-		->addDirectory(__DIR__)
-		->addDirectory(__DIR__ . '/../libs')
-		->register();
-}
+	if ($configurator->isDebugMode()) {
+		$configurator->createRobotLoader()
+			->addDirectory(__DIR__)
+			->addDirectory(__DIR__ . '/../libs')
+			->register();
+	}
 
-$configurator->addConfig(__DIR__ . '/config/config.neon');
-$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+	$configurator->addConfig(__DIR__ . '/config/config.neon');
+	$configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
-return $configurator->createContainer();
+	return $configurator->createContainer();
+});
